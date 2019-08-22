71bc342b13f5ef51edcb5e64cdcf41870dd9d645
Bernhard Arnold <bernhard.arnold@cern.ch>
2019-08-22 12:48:14 +0200

    removed class LinePlotWidget

--- comet/widgets/__init__.py
+++ comet/widgets/__init__.py
@@ -2,6 +2,5 @@ from .application import Application
 from .mainwindow import MainWindow
 from .aboutdialog import AboutDialog
 from .preferencesdialog import PreferencesDialog
-from .lineplotwidget import LinePlotWidget
 from .spinbox import SpinBox
 from .models import BufferTableModel
