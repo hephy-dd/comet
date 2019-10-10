49f97c9a56df90482a063fdf43a72a124af4e83d
Bernhard Arnold <bernhard.arnold@cern.ch>
2019-10-10 14:42:10 +0200

    removed obsolete tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,25 +2,25 @@ import tempfile
 import unittest
 import os
 
-from comet import utilities
+from comet import utils
 
 class UtiltiesTest(unittest.TestCase):
 
     def testPaths(self):
         ref = os.path.abspath(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'comet'))
-        self.assertEqual(utilities.PACKAGE_PATH, ref)
+        self.assertEqual(utils.PACKAGE_PATH, ref)
 
     def testMakePath(self):
-        ref = os.path.join(utilities.PACKAGE_PATH, 'spam', 'ham.txt')
-        self.assertEqual(utilities.make_path('spam', 'ham.txt'), ref)
+        ref = os.path.join(utils.PACKAGE_PATH, 'spam', 'ham.txt')
+        self.assertEqual(utils.make_path('spam', 'ham.txt'), ref)
 
     def testMakeLabel(self):
         ref = 'V max'
-        self.assertEqual(utilities.make_label('v_max'), ref)
+        self.assertEqual(utils.make_label('v_max'), ref)
 
     def testMakeId(self):
         ref = 'nobody_expects_the_spanish_inquisition'
-        self.assertEqual(utilities.make_id('Nobody expects the spanish inquisition!'), ref)
+        self.assertEqual(utils.make_id('Nobody expects the spanish inquisition!'), ref)
 
     def testReplaceExt(self):
         values = (
@@ -30,12 +30,12 @@ class UtiltiesTest(unittest.TestCase):
             (os.path.join('tmp', 'module.py'), '.ui', os.path.join('tmp', 'module.ui')),
         )
         for filename, ext, ref in values:
-            self.assertEqual(utilities.replace_ext(filename, ext), ref)
+            self.assertEqual(utils.replace_ext(filename, ext), ref)
 
     def testSwitchDir(self):
         ref = os.getcwd()
         tmp = tempfile.gettempdir()
-        with utilities.switch_dir(tmp):
+        with utils.switch_dir(tmp):
             self.assertEqual(os.getcwd(), tmp)
         self.assertEqual(os.getcwd(), ref)
 
