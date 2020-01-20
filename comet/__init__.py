303aa09370ef81c950e10032bf1c84ef69944e96
Bernhard Arnold <bernhard.arnold@cern.ch>
2020-01-20 15:49:59 +0100

    removed obsolete module dataseries (replaced by QCharted) #3

--- comet/__init__.py
+++ comet/__init__.py
@@ -2,6 +2,5 @@ from .application import *
 from .ui import *
 from .process import *
 from .device import *
-from .dataseries import *
 from .functions import Range
 from .version import __version__
