b6b4c3d22a85718a7f01794bc2bd29d4d2237789
Bernhard Arnold <bernhard.arnold@burgried.at>
2020-03-09 10:18:25 +0100

    updated utils tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -22,6 +22,10 @@ class UtiltiesTest(unittest.TestCase):
         ref = 'nobody_expects_the_spanish_inquisition'
         self.assertEqual(comet.make_id('Nobody expects the spanish inquisition!'), ref)
 
+    def testMakeIso(self):
+        ref = '2015-02-09T05-39-49'
+        self.assertEqual(comet.make_iso(1423456789.8), ref)
+
     def testEscapeString(self):
         self.assertEqual(comet.escape_string('\r\n\t\\r\\n\\t'), '\\r\\n\\t\\\\r\\\\n\\\\t')
 
@@ -39,11 +43,11 @@ class UtiltiesTest(unittest.TestCase):
             self.assertEqual(comet.replace_ext(filename, ext), ref)
 
     def testSwitchDir(self):
-        ref = os.getcwd()
-        tmp = tempfile.gettempdir()
+        ref = os.path.realpath(os.getcwd())
+        tmp = os.path.realpath(tempfile.gettempdir())
         with comet.switch_dir(tmp):
-            self.assertEqual(os.getcwd(), tmp)
-        self.assertEqual(os.getcwd(), ref)
+            self.assertEqual(os.path.realpath(os.getcwd()), tmp)
+        self.assertEqual(os.path.realpath(os.getcwd()), ref)
 
 if __name__ == '__main__':
     unittest.main()
