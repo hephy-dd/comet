d7afe32b60339de365a5e8e4acb03dbeb27403de
Bernhard Arnold <bernhard.arnold@cern.ch>
2020-06-03 15:07:44 +0200

    added ResourceError

--- comet/resource.py
+++ comet/resource.py
@@ -8,6 +8,43 @@ from .settings import SettingsManager
 
 __all__ = ['Resource', 'ResourceManager', 'ResourceMixin']
 
+class ResourceError(Exception):
+
+    def __init__(self, resource, exc):
+        super(resource.resource_name, resource.visa_library, resource.options, exc)
+
+    @property
+    def resource_name(self):
+        return self.args[0]
+
+    @property
+    def visa_library(self):
+        return self.args[1]
+
+    @property
+    def options(self):
+        return self.args[2]
+
+    @property
+    def exc(self):
+        return self.args[3]
+
+    def __str__(self):
+        return f"{self.resource_name}: {self.exc}"
+
+def lock_resource(f):
+    def lock_resource(self, *args, **kwargs):
+        with self.lock:
+            return f(self, *args, **kwargs)
+
+def resource_errors(f):
+    def resource_errors(self, *args, **kwargs):
+        try:
+            return f(self, *args, **kwargs)
+        except (visa.Error, ConnectionError) as exc:
+            raise ResourceError(self, exc)
+    return resource_errors
+
 class Resource(ContextDecorator):
 
     def __init__(self, resource_name, visa_library=None, **options):
@@ -17,38 +54,53 @@ class Resource(ContextDecorator):
         self.instance = None
         self.lock = threading.RLock()
 
+    @lock_resource
+    @resource_errors
     def __enter__(self):
-        with self.lock:
-            rm = visa.ResourceManager(self.visa_library)
-            if self.instance not in rm.list_opened_resources():
-                self.instance = rm.open_resource(self.resource_name, **self.options)
-            return self
+        rm = visa.ResourceManager(self.visa_library)
+        if self.instance not in rm.list_opened_resources():
+            self.instance = rm.open_resource(self.resource_name, **self.options)
+        return self
 
+    @lock_resource
     def __exit__(self, *exc):
-        with self.lock:
-            if self.instance:
-                self.instance.close()
-                self.instance = None
-            return False
+        if self.instance:
+            self.instance.close()
+            self.instance = None
+        return False
 
+    @lock_resource
+    @resource_errors
     def read(self, *args, **kwargs):
         return self.instance.read(*args, **kwargs)
 
+    @lock_resource
+    @resource_errors
     def read_bytes(self, *args, **kwargs):
         return self.instance.read_bytes(*args, **kwargs)
 
+    @lock_resource
+    @resource_errors
     def read_raw(self, *args, **kwargs):
         return self.instance.read_raw(*args, **kwargs)
 
+    @lock_resource
+    @resource_errors
     def write(self, *args, **kwargs):
         return self.instance.write(*args, **kwargs)
 
+    @lock_resource
+    @resource_errors
     def write_bytes(self, *args, **kwargs):
         return self.instance.write_bytes(*args, **kwargs)
 
+    @lock_resource
+    @resource_errors
     def write_raw(self, *args, **kwargs):
         return self.instance.write_raw(*args, **kwargs)
 
+    @lock_resource
+    @resource_errors
     def query(self, *args, **kwargs):
         return self.instance.query(*args, **kwargs)
 
