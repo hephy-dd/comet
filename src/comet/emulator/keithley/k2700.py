77f4140e32d15ff933c607a25ffd9bbba018d5cf
Bernhard Arnold <bernhard.arnold@oeaw.ac.at>
2024-07-24 14:47:52 +0200

    added K2700 commands

--- src/comet/emulator/keithley/k2700.py
+++ src/comet/emulator/keithley/k2700.py
@@ -12,24 +12,54 @@ class K2700Emulator(IEC60488Emulator):
 
     def __init__(self) -> None:
         super().__init__()
+        self.sense_voltage_average_tcontrol: str = "MOV"
+        self.sense_voltage_average_count: int = 10
+        self.sense_voltage_average_state: bool = False
         self.error_queue: List[Error] = []
         self.system_beeper_state: bool = True
+        self.trigger_delay_auto: bool = True
+        self.trigger_delay: float = 0.001
 
     @message(r'^\*RST$')
     def set_rst(self):
+        self.sense_voltage_average_tcontrol = "MOV"
+        self.sense_voltage_average_count = 10
+        self.sense_voltage_average_state = False
         self.error_queue.clear()
         self.system_beeper_state = True
+        self.trigger_delay_auto = True
+        self.trigger_delay = 0.001
 
     @message(r'^\*CLS$')
     def set_cls(self):
         self.error_queue.clear()
 
-    @message(r'^:?SYST:ERR:COUN\?$')
-    def get_system_error_count(self):
-        return format(len(self.error_queue), "d")
+    @message(r'^:?SENS:VOLT:AVER:TCON\?$')
+    def get_sense_average_tcontrol(self):
+        return self.sense_voltage_average_tcontrol
 
-    @message(r'^:?SYST:ERR(?::NEXT)?\?$')
-    def get_system_error_next(self):
+    @message(r'^:?SENS:VOLT:AVER:TCON\s+(REP|MOV)$')
+    def set_sense_average_tcontrol(self, name: str):
+        self.sense_voltage_average_tcontrol = name
+
+    @message(r'^:?SENS:VOLT:AVER:COUN[T]?\?$')
+    def get_sense_average_count(self):
+        return format(self.sense_voltage_average_count, "d")
+
+    @message(r'^:?SENS:VOLT:AVER:COUN[T]?\s+(\d+)$')
+    def set_sense_average_count(self, count: str):
+        self.sense_voltage_average_count = int(count)
+
+    @message(r'^:?SENS:VOLT:AVER(?::STAT[E]?)?\?$')
+    def get_sense_voltage_average_state(self):
+        return format(self.sense_voltage_average_state, "d")
+
+    @message(r'^:?SENS:VOLT:AVER(?::STAT[E]?)?\s+(OFF|ON|0|1)$')
+    def set_sense_voltage_average_state(self, value):
+        self.sense_voltage_average_state = {"0": False, "1": True, "OFF": False, "ON": True}[value]
+
+    @message(r'^:?SYST:ERR\?$')
+    def get_system_error(self):
         if self.error_queue:
             error = self.error_queue.pop(0)
         else:
@@ -44,7 +74,7 @@ class K2700Emulator(IEC60488Emulator):
     def set_beeper_state(self, value):
         self.system_beeper_state = {'0': False, '1': True, 'OFF': False, 'ON': True}[value]
 
-    @message(r'^:?INIT$')
+    @message(r'^:?INIT(?::IMM)$')
     def set_init(self):
         ...
 
@@ -57,6 +87,27 @@ class K2700Emulator(IEC60488Emulator):
         vdc = random.uniform(.00025, .001)
         time.sleep(random.uniform(.5, 1.0))  # rev B10 ;)
         return "{:E},+0.000,+0.000,+0.000,+0.000".format(vdc)
+
+    # Trigger
+
+    @message(r':?TRIG:DEL:AUTO\?')
+    def get_trigger_delay_auto(self):
+        return format(self.trigger_delay_auto, "d")
+
+    @message(r':?TRIG:DEL:AUTO\s+(OFF|ON|0|1)')
+    def set_trigger_delay_auto(self, value):
+        self.trigger_delay_auto = {'0': False, '1': True, 'OFF': False, 'ON': True}[value]
+
+    @message(r':?TRIG:DEL\?')
+    def get_trigger_delay(self):
+        return format(self.trigger_delay, "E")
+
+    @message(r':?TRIG:DEL\s+(.+)')
+    def set_trigger_delay(self, value):
+        try:
+            self.trigger_delay = min(999999.999, max(0.001, float(value)))
+        except ValueError:
+            self.error_queue.append(Error(-113, "undefined header"))
 
     @message(r'^(.*)$')
     def unknown_message(self, request):
