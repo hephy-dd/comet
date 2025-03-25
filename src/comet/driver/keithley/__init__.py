de5f8762eaebb20e237f03132c67397daa3be116
Bernhard Arnold <bernhard.arnold@oeaw.ac.at>
2025-03-25 15:38:29 +0100

    fixed import, changelog

--- src/comet/driver/keithley/__init__.py
+++ src/comet/driver/keithley/__init__.py
@@ -5,6 +5,7 @@ from .k2400 import K2400
 from .k2410 import K2410
 from .k2470 import K2470
 from .k2657a import K2657A
+from .k6510 import K6510
 from .k6514 import K6514
 from .k6517b import K6517B
 
@@ -16,6 +17,7 @@ __all__ = [
     "K2410",
     "K2470",
     "K2657A",
+    "K6510",
     "K6514",
     "K6517B",
 ]
