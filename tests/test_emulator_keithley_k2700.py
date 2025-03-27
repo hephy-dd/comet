7eadd472ef23e3a8050730a7a19c7d93197bfd81
Bernhard Arnold <bernhard.arnold@oeaw.ac.at>
2025-03-27 17:33:00 +0100

    fixed K2700 reading format

--- tests/test_emulator_keithley_k2700.py
+++ tests/test_emulator_keithley_k2700.py
@@ -12,3 +12,18 @@ def test_basic(emulator):
     assert emulator("*IDN?") == "Keithley Inc., Model 2700, 43768438, v1.0 (Emulator)"
     assert emulator("*RST") is None
     assert emulator("*OPC?") == "1"
+
+
+def test_format_elements(emulator):
+    assert emulator(":FORM:ELEM?") == "READ,,UNIT,RNUM,TST,"
+    assert emulator(":FORM:ELEM READ") is None
+    assert emulator(":FORM:ELEM?") == "READ,,,,,"
+    assert emulator(":FORM:ELEM CHAN, CHAN ,UNIT") is None
+    assert emulator(":FORM:ELEM?")  == ",CHAN,UNIT,,,"
+    assert emulator(":FORM:ELEM CHAN,TST,READ,UNIT,LIM,LIM,RNUM") is None
+    assert emulator(":FORM:ELEM?") == "READ,CHAN,UNIT,RNUM,TST,LIM"
+
+
+def test_reading(emulator):
+    assert emulator(":FORM:ELEM READ") is None
+    assert emulator(":READ?") is not None
