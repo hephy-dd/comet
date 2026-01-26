afea605c2a736c6d5a62cb28e66f4e5083ddb4f7
Bernhard Arnold <bernhard.arnold@oeaw.ac.at>
2026-01-26 15:21:13 +0100

    refs #106 added PilasGui driver

--- src/comet/driver/hephy/__init__.py
+++ src/comet/driver/hephy/__init__.py
@@ -1,5 +1,6 @@
 from .brandbox import BrandBox
 from .environbox import EnvironBox
+from .pilasgui import PilasGui
 from .shuntbox import ShuntBox
 
-__all__ = ["BrandBox", "EnvironBox", "ShuntBox"]
+__all__ = ["BrandBox", "EnvironBox", "PilasGui", "ShuntBox"]
