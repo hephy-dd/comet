6938c9a3bf4915d7bc7935bc5bda39cb89c89359
Bernhard Arnold <arnobaer@users.noreply.github.com>
2026-03-18 14:23:40 +0100

    feature(ac3): add temperature gradient logic to emulator (#116)
    Changing target temperature now updates readings over time using a simple gradient.

--- tests/test_emulator_ers_ac3.py
+++ tests/test_emulator_ers_ac3.py
@@ -1,6 +1,6 @@
 import pytest
 
-from comet.emulator.ers.ac3 import AC3Emulator
+from comet.emulator.ers.ac3 import AC3Emulator, Mode
 
 
 @pytest.fixture
@@ -9,7 +9,7 @@ def emulator():
 
 
 def test_identify(emulator):
-    assert emulator("RI") == "I1"
+    assert emulator("RI") == "I0"
 
 
 def test_get_temperature(emulator):
@@ -22,10 +22,10 @@ def test_get_target_temperature(emulator):
 
 def test_set_target_temperature(emulator):
     assert emulator("ST+0300") == "OK"
-    assert emulator.target_temperature == 30.0
+    assert emulator.state.target_temperature == 30.0
 
     assert emulator("ST-0300") == "OK"
-    assert emulator.target_temperature == -30.0
+    assert emulator.state.target_temperature == -30.0
 
 
 def test_get_mode(emulator):
@@ -34,7 +34,7 @@ def test_get_mode(emulator):
 
 def test_set_mode(emulator):
     assert emulator("SO2") == "OK"
-    assert emulator.mode == 2
+    assert emulator.state.mode == Mode.STANDBY
 
 
 def test_get_dewpoint(emulator):
@@ -47,10 +47,10 @@ def test_get_dewpoint_control_status(emulator):
 
 def test_set_dewpoint_control_status(emulator):
     assert emulator("SD0") == "OK"
-    assert not emulator.dewpoint_control_status
+    assert not emulator.state.dewpoint_control_status
 
     assert emulator("SD1") == "OK"
-    assert emulator.dewpoint_control_status
+    assert emulator.state.dewpoint_control_status
 
 
 def test_get_hold_mode(emulator):
@@ -59,14 +59,18 @@ def test_get_hold_mode(emulator):
 
 def test_set_hold_mode(emulator):
     assert emulator("SH1") == "OK"
-    assert emulator.hold_mode == 11
+    assert emulator.state.hold_mode == 11
 
     assert emulator("SH0") == "OK"
-    assert emulator.hold_mode == 0
+    assert emulator.state.hold_mode == 0
 
 
 def test_get_control_status(emulator):
+    assert emulator("RI") == "I0"
+    assert emulator("ST+0300") == "OK"
     assert emulator("RI") == "I1"
+    assert emulator("ST-0300") == "OK"
+    assert emulator("RI") == "I2"
 
 
 def test_get_error_code(emulator):
